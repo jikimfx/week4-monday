452af74d51c49b9c3f187fd5260092f48092c992
jikimfx <jikimfx@gmail.com>
2020-05-27 05:36:48 +0700

    Rock Paper Scissor Part Deux

--- src/App.js
+++ src/App.js
@@ -1,28 +1,63 @@
-import React from 'react';
-import './App.css';
-import ChoiceCard from './components/ChoiceCard';
+import React, { useState } from "react";
+import "./App.css";
 
-const choices = {
-  rock:
-    "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
-  paper: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
-  scissors: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
-};
+import ChoiceCard from "./components/ChoiceCard";
+import ChoiceButtons from "./components/ChoiceButtons";
+
+import { CHOICES, getRoundOutcome } from "./utils";
 
 function App() {
+  const [prompt, setGamePrompt] = useState("1, 2, 3, SHOOT!");
+  const [gameHistory, setGameHistory] = useState([]);
+  const [previousWinner, setPreviousWinner] = useState(null);
+  const [playerChoice, setPlayerChoice] = useState(null);
+  const [computerChoice, setComputerChoice] = useState(null);
+
+  const onPlayerChoose = playerChoice => {
+    const [result, compChoice] = getRoundOutcome(playerChoice);
+
+    const newUserChoice = CHOICES[playerChoice];
+    const newComputerChoice = CHOICES[compChoice];
+
+    setGamePrompt(result);
+    gameHistory.push(result);
+    setGameHistory(gameHistory);
+    setPlayerChoice(newUserChoice);
+    setComputerChoice(newComputerChoice);
+
+    if (result === "Victory!") setPreviousWinner("You");
+    else if (result === "Defeat!") setPreviousWinner("Computer");
+    else setPreviousWinner("Tie");
+  };
+
   return (
     <div className="App">
-      <ChoiceCard
-        title="You"
-        winner={true}
-        imgURL={choices.scissors}
-      />
-
-      <ChoiceCard
-        title="Computer"
-        winner={false}
-        imgURL={choices.paper}
-        />
+      <div className="container">
+        <div className="row mb-3">
+          <div className="col-md-8 themed-grid-col">
+            <ChoiceCard
+              title="Computer"
+              imgURL={computerChoice && computerChoice.url}
+              previousWinner={previousWinner}
+            />
+            <h1>{prompt}</h1>
+            <ChoiceButtons onPlayerChoose={onPlayerChoose} />
+            <ChoiceCard
+              title="You"
+              previousWinner={previousWinner}
+              imgURL={playerChoice && playerChoice.url}
+            />
+          </div>
+          <div className="col-md-4 themed-grid-col">
+            <h3>History</h3>
+            <ul>
+              {gameHistory.map(result => {
+                return <li>{result}</li>;
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
